702d056e6b73362b3dd29f9dff7c575e39bf5197
gregsugiyama <gjsugiyama@gmail.com>
2022-12-02 22:01:17 -0800

    solves day 2

---
+++ src/gjs/2022/day_2.clj
@@ -0,0 +1,32 @@
+(ns gjs.2022.day-2
+  (:require
+   [clojure.string :refer [split]]
+   [util.santas-little-helpers :refer [read-input]]))
+(def input (->> (read-input "gjs/2022/day_2.txt")
+                (map #(split % #"\s"))))
+(def scores-pt-1 {["A" "X"] 4
+                  ["A" "Y"] 8
+                  ["A" "Z"] 3
+                  ["B" "X"] 1
+                  ["B" "Y"] 5
+                  ["B" "Z"] 9
+                  ["C" "X"] 7
+                  ["C" "Y"] 2
+                  ["C" "Z"] 6})
+(def scores-pt-2 {["A" "X"] 3
+                  ["A" "Y"] 4
+                  ["A" "Z"] 8
+                  ["B" "X"] 1
+                  ["B" "Y"] 5
+                  ["B" "Z"] 9
+                  ["C" "X"] 2
+                  ["C" "Y"] 6
+                  ["C" "Z"] 7})
+
+(defn solve
+  [scores]
+  (transduce (map #(get scores %)) + 0 input))
+
+(comment
+  (solve scores-pt-1)
+  (solve scores-pt-2))
